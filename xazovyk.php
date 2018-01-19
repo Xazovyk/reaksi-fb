45d7d6525e198f39b4f4ef6b5ed91daddaf8e566
Dedi Rohadi <35531852+Xazovyk@users.noreply.github.com>
2018-01-19 01:28:03 +0000

    Update xazovyk.php

--- xazovyk.php
+++ xazovyk.php
@@ -2,13 +2,27 @@
 
 require_once('fb.php');
 
-//////////////////////////////////////////////[edit di sini]///////////////////////////////////////////
+echo "\n\e[93m
+ __   __                                                    
+/\ \ /\ \                                                   
+\ `\`\/'/'      __      ____      ___    __  __   __  __    
+ `\/ > <      /'__`\   /\_ ,`\   / __`\ /\ \/\ \ /\ \/\ \   
+    \/'/\`\  /\ \L\.\_ \/_/  /_ /\ \L\ \\ \ \_/ |\ \ \_\ \  
+    /\_\\ \_\\ \__/.\_\  /\____\\ \____/ \ \___/  \/`____ \ 
+    \/_/ \/_/ \/__/\/_/  \/____/ \/___/   \/__/    `/___/> \
+                          
+                                           
+\e[36m▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄\n\e[36m
+Github  : http://github.com/Xazovyk
+
+\e[36m▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄";
+echo "\n\e";
 $user		= 'email'; // facebook username / email
 $pass 		= 'kata sandi'; // facebook passwod
-$r_male		= '3'; // reaction if user male , like = 1, love = 2, wow = 3, haha = 4, sad = 7, angry = 8
-$r_female	= '2'; // reaction if user female , like = 1, love = 2, wow = 3, haha = 4, sad = 7, angry = 8,
-$max_status	= '100'; // maximum reacted status
-$token 		= 'EAAAAAYsX7TsBACA1P2JsYupRLoWoUEG7ZACtRtFVQSoNv8shknjqwjU6K6zWtiXsiyv7ZAFLMsTZA2bPP6DdvrqbJMs33AU3FLs8tOFFCr9AVQ5DMS7kNRtMKKFnf2qB8TSMkY8hZBQ43zW2205kCellSiI6QakIoXwpl6LEJwJSibY1oZBNWL6ZAX9qULt7Ci1tEZAXvxTVTcgySwE';
+$r_male		= '1'; // reaction if user male , like = 1, love = 2, wow = 3, haha = 4, sad = 7, angry = 8
+$r_female	= '1'; // reaction if user female , like = 1, love = 2, wow = 3, haha = 4, sad = 7, angry = 8,
+$max_status	= '400'; // maximum reacted status
+$token 		= 'EAAAAAYsX7TsBACAkdUw1P2JsYupRLoWoUEG7ZACtRtFVQSoNv8shknjqwjU6K6zWtiXsiyv7ZAFbPP6DdvrqbJMs33AU3FLs8tOFFCr9AVQ5DMS7kNRtMKKFnf2qB8TSMkY8hZBQ43zW2205kCellSiI6QakIoXwpl6LEJwJSibY1oZBNWL6ZAX9qULt7Ci1tEZAXvxTVTcgySwE';
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 $config['cookie_file'] = 'cookie.txt';
